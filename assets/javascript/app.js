32e3b5b6b6ca4513faadc4c003c6dc37a9fc8c15
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2018-12-19 19:03:19 -0700

    Added start button and more styling.  Added 11 questions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,24 +8,105 @@ var gameShow = {
     time: 24,
     questionsRight: 0,
     questionsWrong: 0,
-    numberOfQuestions: 2,
+    numberOfQuestions: 11,
     question1: {
-        question: "This is question one.",
+        question: "When did Colorado become a state?",
         answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
         correctAnswer: "4",
-        choice1: "Sample choice one.",
-        choice2: "Sample choice two.",
-        choice3: "Sample choice three.",
-        choice4: "The correct choice."
+        choice1: "1870",
+        choice2: "1901",
+        choice3: "1856",
+        choice4: "1876"
     },
     question2: {
-        question: "This is question two.",
+        question: "Who was the first person to summit Pikes Peak?",
         answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
         correctAnswer: "2",
-        choice1: "Sample choice one.",
-        choice2: "The correct choice.",
-        choice3: "Sample choice three.",
-        choice4: "Sample choice two."
+        choice1: "James Connor",
+        choice2: "Edwin James",
+        choice3: "Zebulon Pike",
+        choice4: "Edmund Hillary"
+    },
+    question3: {
+        question: "What is Colorado's state fossil?",
+        answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
+        correctAnswer: "1",
+        choice1: "Stegosaurus",
+        choice2: "Triceratops",
+        choice3: "T-Rex",
+        choice4: "Apatasaurus"
+    },
+    question4: {
+        question: "What is the tallest mountain in Colorado?",
+        answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
+        correctAnswer: "2",
+        choice1: "Pikes Peak",
+        choice2: "Mount Elbert",
+        choice3: "Longs Peak",
+        choice4: "Mount Castle"
+    },
+    question5: {
+        question: "Which Colorado city is known as the 'Choice City'?",
+        answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
+        correctAnswer: "3",
+        choice1: "Boulder",
+        choice2: "Colorado Springs",
+        choice3: "Fort Collins",
+        choice4: "Denver"
+    },
+    question6: {
+        question: "What is Colorado's official state dance?",
+        answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
+        correctAnswer: "4",
+        choice1: "The Waltz",
+        choice2: "The Two-Step",
+        choice3: "Line Dance",
+        choice4: "Square Dance"
+    },
+    question7: {
+        question: "Where does Colorado get its name?",
+        answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
+        correctAnswer: "1",
+        choice1: "The red soil",
+        choice2: "Native Americans",
+        choice3: "Fransico Colorado",
+        choice4: "An early fort in the area"
+    },
+    question8: {
+        question: "What was the original name for the Colorado River?",
+        answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
+        correctAnswer: "4",
+        choice1: "Amazing River",
+        choice2: "Red River",
+        choice3: "Roaring Fork",
+        choice4: "Grand River"
+    },
+    question9: {
+        question: "Which city is near the lowest elevation in Colorado?",
+        answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
+        correctAnswer: "2",
+        choice1: "Stirling",
+        choice2: "Wray",
+        choice3: "Pueblo",
+        choice4: "Rocky Ford"
+    },
+    question10: {
+        question: "What does the blue color in Colorado's state flag stand for?",
+        answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
+        correctAnswer: "3",
+        choice1: "The Lakes resevoirs",
+        choice2: "Nothing",
+        choice3: "The blue sky",
+        choice4: "The blue corn"
+    },
+    question11: {
+        question: "What Colorado mountains appear on the Coors beer can?",
+        answerChoices: ["Sample choice one.", "Sample choice two.", "Sample choice three.", "The correct choice."],
+        correctAnswer: "2",
+        choice1: "Pikes Peak",
+        choice2: "The Maroon Bells",
+        choice3: "Quandry Peak",
+        choice4: "Mount Evans"
     },
     displayQuestion() {
         if (gameShow.numberOfQuestions === 0) {
@@ -54,31 +135,9 @@ var gameShow = {
                 if (buttonNumber === currentQuestion.correctAnswer) {
                     //trigger win
                     gameShow.triggerWin();
-                    // clearInterval(intervalId);
-                    // gameShow.questionsRight++;
-                    // $("#timerText").text(" ");
-                    // var randomImgNum = Math.floor(Math.random() * 2);
-                    // var winImg = $("<img>").addClass("fluid-img thumbnail-img d-block mx-auto").attr("src", "assets/images/win1.png");
-                    // $("#imgCol").append(winImg);
-                    // $("."+ buttonNumber).attr("border", "solid red");
-                    // setTimeout(function () {
-                    //     $("#imgCol").empty();
-                    //     gameShow.displayQuestion();
-                    // }, 5000);
 
                 } else {
                     gameShow.triggerLoss();
-                    // clearInterval(intervalId);
-                    // clearInterval(intervalId);
-                    // gameShow.questionsWrong++;
-                    // $("#timerText").text(" ");
-                    // var randomImgNum = Math.floor(Math.random() * 2);
-                    // var lossImg = $("<img>").addClass("fluid-img thumbnail-img d-block mx-auto").attr("src", "assets/images/fail.jpg");
-                    // $("#imgCol").append(lossImg);
-                    // setTimeout(function () {
-                    //     $("#imgCol").empty();
-                    //     gameShow.displayQuestion();
-                    // }, 5000);
                 }
             })
             questionNumber++;
@@ -121,7 +180,6 @@ var gameShow = {
     },
     triggerLoss: function () {
         clearInterval(intervalId);
-        clearInterval(intervalId);
         gameShow.questionsWrong++;
         $("#timerText").text(" ");
         var randomImgNum = Math.floor(Math.random() * 2);
@@ -142,3 +200,8 @@ var gameShow = {
     }
 
 }
+
+$("#start").on("click", function(){
+    $("#start").remove();
+    gameShow.displayQuestion();
+})
